bfabe04162eb1c0b1ccbecb122428f1a0988992b
Traci Crawford <tracicrawford@Tracis-Air.5-A-Rm511>
2019-11-21 17:49:25 -0600

    html complete

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -2,37 +2,51 @@
 
 // Create a variable called 'myName' with a value that is your name. 
 
-// Code here
+var myName = 'Traci';
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' with a value that is your favorite number. 
 
-//Code here
+var faveNum = 5;
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and assign it a value of true.
 
-//Code here
+var lovesCode = Boolean (true);
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. The function should return the sum of the two parameters.
 
-//Code here
+function sum (num1, num2) {
+	return num1 + num2;
+}
+
+
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called 'x'. Check to see if 'x' is equal to true. If it is, return a true boolean. If 'x' does not equal true, return a false boolean.  
 
-//Code here
+function lovesCodeChecker(x) {
+if (x==="true") {
+	return true;
+} else {
+	return false;
+}
+}
 
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, 'num'. Your function should check to see if the parameter is even or odd. If the parameter is odd, return 'the number is odd'. If the number is even, return 'the number is even'.
 
-//Code here
+function oddChecker (num) {
+	for (var i = 0; i < Array.length; itt){
+		
+	}
+}
 
 //////////////////PROBLEM 7////////////////////
 
